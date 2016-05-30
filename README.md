4e70464fd9c40021391f6b2689c48198f479a3c1
root <root@localhost.localdomain>
2016-05-30 08:33:58 -0400

    Python String Encoder

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # String-Encoder
+
+Python String encoder/decoder
---
+++ encode.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+str = "this is string example....wow!!!";
+
+print "Encoded String: " + str.encode('base64','strict')
